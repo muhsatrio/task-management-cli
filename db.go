b7227c53e86119ec6536f1622e0761750fdb19ac
muhsatrio <muhsatrio@gmail.com>
2020-08-30 16:12:28 +0700

    feat(task): update filter task and change completed task

--- db.go
+++ db.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -13,10 +14,12 @@ import (
 
 // Task struct
 type Task struct {
-	ID          int    `json:"id"`
-	Rev         int    `json:"rev"`
+	ID          string `json:"_id"`
+	Rev         string `json:"_rev"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+	Tags        []Tag  `json:"tags"`
 	CreatedAt   string `json:"createdAt"`
 }
 
@@ -36,6 +39,12 @@ type ResponseAPI struct {
 	Rows      []Docs `json:"rows"`
 }
 
+// Tag struct
+type Tag struct {
+	ID    string `json:"id"`
+	Value string `json:"value"`
+}
+
 var baseURL = getURLAPI()
 var listTasks []Task
 
@@ -48,9 +57,33 @@ func getURLAPI() string {
 
 	user := os.Getenv("USER_DB")
 	password := os.Getenv("PASSWORD_DB")
-	fmt.Println(user, password)
 
 	return "http://" + user + ":" + password + "@13.250.43.79:5984"
+}
+
+func getTask(id string) Task {
+	i := 0
+	for i < len(listTasks) {
+		if listTasks[i].ID == id {
+			break
+		}
+		i++
+	}
+	if i >= len(listTasks) {
+		log.Fatalln("error: not found")
+		os.Exit(0)
+	}
+	return listTasks[i]
+}
+
+func filterTask(completed bool) []Task {
+	var filteredTask []Task
+	for i := 0; i < len(listTasks); i++ {
+		if listTasks[i].Completed == completed {
+			filteredTask = append(filteredTask, listTasks[i])
+		}
+	}
+	return filteredTask
 }
 
 func initTasks() {
@@ -58,6 +91,7 @@ func initTasks() {
 
 	if err != nil {
 		log.Fatalln(err)
+		os.Exit(0)
 	}
 
 	defer resp.Body.Close()
@@ -66,10 +100,26 @@ func initTasks() {
 
 	bodyString := string(bodyBytes)
 
-	fmt.Println(bodyString)
-
 	var responseAPI ResponseAPI
 	json.Unmarshal([]byte(bodyString), &responseAPI)
-	fmt.Println(responseAPI)
+	listTasks = nil
+	for _, doc := range responseAPI.Rows {
+		resp, _ = http.Get(baseURL + "/efishery_task/" + doc.ID)
+		bodyBytes, _ = ioutil.ReadAll(resp.Body)
+		bodyString = string(bodyBytes)
+		var taskTemp Task
+		json.Unmarshal([]byte(bodyString), &taskTemp)
+		listTasks = append(listTasks, taskTemp)
+	}
+}
 
+func changeTask(id string, rev string, newTask Task) {
+	json, _ := json.Marshal(newTask)
+	fmt.Println(json)
+	_, err := http.NewRequest(http.MethodPut, "/efishery_task/"+id+"?rev="+rev, bytes.NewBuffer(json))
+
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(0)
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func listCommand() {
@@ -9,28 +12,68 @@ func listCommand() {
 	fmt.Println("List command available:")
 	fmt.Println("1. task-todo : to find out todo task that still not be done")
 	fmt.Println("2. task-done : to find out task that had been done")
-	fmt.Println("3. check-task --id (id of task) --rev (rev of task) : move task from todo to done")
+	fmt.Println("3. check-task : move task from todo to done")
 	fmt.Println("========================")
 }
 
 func main() {
 	initTasks()
-	// if len(os.Args) < 2 {
-	// 	fmt.Println("Error: subcommand is required")
-	// 	listCommand()
-	// 	os.Exit(1)
-	// }
 
-	// switch os.Args[1] {
-	// case "task-todo":
-	// 	resp, err := http.Get(baseURL + "/efishery_task")
-	// case "task-done":
-	// 	fmt.Println("task-done")
-	// case "check-task":
-	// 	fmt.Println("check-task")
-	// default:
-	// 	fmt.Println("Error: subcommand is wrong")
-	// 	listCommand()
-	// 	os.Exit(1)
-	// }
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Error: subcommand is required")
+		listCommand()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "task-todo":
+		initTasks()
+		filteredTask := filterTask(false)
+		if len(filteredTask) > 0 {
+			for i, task := range filteredTask {
+				fmt.Printf("%d. %s\n", i+1, task.Name)
+				fmt.Printf("ID: %s\n", task.ID)
+				fmt.Printf("Rev: %s\n", task.Rev)
+				fmt.Printf("Description: %s\n", task.Description)
+			}
+		} else {
+			fmt.Println("Task is empty")
+			os.Exit(0)
+		}
+		// fmt.Println(foundTask)
+	case "task-done":
+		initTasks()
+		filteredTask := filterTask(true)
+		if len(filteredTask) > 0 {
+			for i, task := range filteredTask {
+				fmt.Printf("%d. %s\n", i+1, task.Name)
+				fmt.Printf("ID: %s\n", task.ID)
+				fmt.Printf("Rev: %s\n", task.Rev)
+				fmt.Printf("Description: %s\n", task.Description)
+			}
+		} else {
+			fmt.Println("Task is empty")
+			os.Exit(0)
+		}
+	case "check-task":
+		fmt.Print("Enter ID: ")
+		scanner.Scan()
+		idInputed := scanner.Text()
+		fmt.Print("Enter Rev: ")
+		scanner.Scan()
+		revInputed := scanner.Text()
+		if len(idInputed) == 0 || len(revInputed) == 0 {
+			log.Fatalln("ID or Rev is required")
+		}
+		initTasks()
+		foundTask := getTask(idInputed)
+		foundTask.Completed = true
+		changeTask(idInputed, revInputed, foundTask)
+	default:
+		fmt.Println("Error: subcommand is wrong")
+		listCommand()
+		os.Exit(1)
+	}
 }
